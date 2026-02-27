c5d92b88901f9305d87b25ef6ebfe0ef262374f3
denish1890 <rakholiyadenish@gmail.com>
2026-02-28 01:56:38 +0530

    Update p.py

--- p.py
+++ p.py
@@ -27,7 +27,7 @@ try:
         host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
         port=4000,
         user="4Er7E7yAa5CmneH.root",
-        password=st.secrets["tidb_password"],
+        password="JubMX8vnCyJqhX96",
         database="cafe",
         ssl_verify_identity=True,
         ssl_ca="/etc/ssl/certs/ca-certificates.crt"
@@ -979,6 +979,7 @@ elif st.session_state["page"] == "downloadbill":
      pdf.output(file_name)
 
      st.success("Bill saved to your system!")
+
 
 
 
